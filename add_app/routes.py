365e9e1cf74d5b61e5534f2699d9122531e33634
Ronald Kemei <kemeirnld@gmail.com>
2024-05-21 12:18:56 +0300

    Add fuature to add two intergers and add route

---
+++ add_app/routes.py
@@ -0,0 +1,10 @@
+from . import addition_bp
+from flask import request, jsonify
+from .add import add
+
+@addition_bp.route('/add',methods=['GET'])
+def add_route():
+    num1 = int(request.args.get('num1',0))
+    num2 = int(request.args.get('num2',0))
+    result = add(num1,num2)
+    return jsonify({'result':result})
